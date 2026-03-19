620ec5f991e3ee1d34836607d8b62df3955143b4
NIKESH S <nikesh06042004@gmail.com>
2026-03-19 12:34:59 +0530

    env updated

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// const API_BASE_URL = 'https://nestcart.onrender.com';
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
-const API_BASE_URL = 'http://localhost:8081';
+// const API_BASE_URL = 'http://localhost:8081';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
